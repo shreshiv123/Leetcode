dbc4a95ce1217a3713dfa7473a8592e4f1e7709e
Shreya Singh <singhshreyu.123@gmail.com>
2023-08-02 23:20:11 +0530

    Added solution - LeetHub

---
+++ Shortest Source to Destination Path - GFG/shortest-source-to-destination-path.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int shortestDistance(int N, int M, vector<vector<int>> A, int X, int Y) {
+        // code here
+        vector<vector<int>> vis(N,vector<int> (M,0));
+        queue<pair<pair<int,int>,int>>q;
+        q.push({{0,0},0});
+        vis[0][0]=1;
+        int delrow[4]={-1,0,1,0};
+        int delcol[4]={0,1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int steps=q.front().second;
+            q.pop();
+            if(row==X && col==Y) return steps;
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<N && ncol<M && A[nrow][ncol]==1 && !vis[nrow][ncol]){
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},steps+1});
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M, x, y;
+        cin >> N >> M;
+        vector<vector<int>> v(N, vector<int>(M));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++) cin >> v[i][j];
+        cin >> x >> y;
+        Solution ob;
+        cout << ob.shortestDistance(N, M, v, x, y) << "\n";
+    }
+}
+// } Driver Code Ends
